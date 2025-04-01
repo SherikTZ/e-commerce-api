3500a9287a464b123afc9618a3223b3df57d8b7c
SherikTZ <shervintursunzade@gmail.com>
2025-04-01 00:04:27 -0400

    Change Product model to work with lombok

--- product/src/main/java/com/ecommerce/product/Product.java
+++ product/src/main/java/com/ecommerce/product/Product.java
@@ -10,7 +10,13 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.PositiveOrZero;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
 
+@Getter
+@Setter
+@NoArgsConstructor
 @Entity
 @Table(name = "products")
 public class Product {
@@ -42,59 +48,4 @@ public class Product {
 
     private Boolean isActive;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(@NotBlank @Max(NAME_MAX) String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(@Max(DESC_MAX) String description) {
-        this.description = description;
-    }
-
-    public Float getPrice() {
-        return price;
-    }
-
-    public void setPrice(@NotNull @Positive Float price) {
-        this.price = price;
-    }
-
-    public Integer getStock() {
-        return stock;
-    }
-
-    public void setStock(@NotNull @PositiveOrZero Integer stock) {
-        this.stock = stock;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(@NotBlank String category) {
-        this.category = category;
-    }
-
-    public Boolean getIsActive() {
-        return isActive;
-    }
-
-    public void setIsActive(Boolean isActive) {
-        this.isActive = isActive;
-    }
 }
